b18014e997970ccb7d25ebcf97d323f48a00955d
fawazmez <36240699+fawazmez@users.noreply.github.com>
2018-02-28 23:32:32 -0500

    Initial commit

---
+++ AreaAndCircumfrance/main.lua
@@ -0,0 +1,55 @@
+-----------------------------------------------------------------------------------------
+--
+-- main.lua
+-- 
+-- Created by : Fawaz Mezher
+-- Created on : Feb 2018
+-- 
+-- Calculates the circumfrance and area of the circle
+-----------------------------------------------------------------------------------------
+--all the variables
+
+--this shows the names of which value we should put in each textbox
+local InstructionTextField = display.newText( "Enter the radius in the box", display.contentCenterX - 800, display.contentCenterY - 700, native.systemFont, 95 )
+
+-- the text boxes
+local DiameterTextField = native.newTextField( display.contentCenterX - 800, display.contentCenterY - 300, 750, 150 )
+DiameterTextField.id = "length textField"
+
+--area and circumfrance text
+local AreaTextField = display.newText( "Area", display.contentCenterX - 400, display.contentCenterY + 200, native.systemFont, 100 )
+AreaTextField.id = "AreaTextField"
+AreaTextField:setFillColor( 1, 1, 1 )
+
+local CircumfranceTextField = display.newText( "Circumfrance", display.contentCenterX - 400, display.contentCenterY + 500, native.systemFont, 100 )
+CircumfranceTextField.id = "CircumfranceTextField"
+CircumfranceTextField:setFillColor( 1, 1, 1 )
+
+-- the click button
+local calculateButton = display.newImageRect( "./assets/sprites/enterButton.png", 406, 157 )
+calculateButton.x = display.contentCenterX +1000
+calculateButton.y = display.contentCenterY +800
+calculateButton.id = "calculate button"
+
+local function calculateButtonTouch( event )
+    -- this function calculates the area and circumfrance
+ 
+    local Diameter
+	local Area 
+	local Circumfrance
+	
+    
+    Diameter = DiameterTextField.text
+    Area = AreaTextField.text
+    Circumfrance = CircumfranceTextField.text
+  	Area = math.pi * Diameter^2
+	Circumfrance = 2 * math.pi * Diameter
+	-- print( area and circumfrance )
+   AreaTextField.text = "The area is " .. Area.."cm"
+   CircumfranceTextField.text = "The circumfrance is " ..Circumfrance.."cm"
+
+
+    return true
+end
+
+calculateButton:addEventListener( "touch", calculateButtonTouch )
---
+++ areaOfTrapzoid/main.lua
@@ -0,0 +1,63 @@
+-----------------------------------------------------------------------------------------
+--
+-- main.lua
+-- 
+-- Created by : Fawaz Mezher
+-- Created on : Feb 2018
+-- 
+-- how to calculate the area of a trapazoid
+-----------------------------------------------------------------------------------------
+--all the variables
+
+--this shows the names of which value we should put in each textbox
+local InstructionTextField = display.newText( "Enter each value in it respected box.", display.contentCenterX - 500, display.contentCenterY - 700, native.systemFont, 95 )
+
+local lengthOfBaseATextField = display.newText( "Base a", display.contentCenterX - 800, display.contentCenterY - 300, native.systemFont, 90 )
+lengthOfBaseATextField:setFillColor( 99, 10, 88 )
+
+local lenghtOfBaseBTextField = display.newText( "Base b", display.contentCenterX - 800, display.contentCenterY - 100, native.systemFont, 90 )
+lenghtOfBaseBTextField:setFillColor( 99, 10, 88 )
+
+local lenghtOfHeightTextField = display.newText( "height", display.contentCenterX - 800, display.contentCenterY +100, native.systemFont, 100 )
+lenghtOfHeightTextField:setFillColor( 99, 10, 88 )
+-- the text boxes
+local lengthOfBaseATextField = native.newTextField( display.contentCenterX - 400, display.contentCenterY - 300, 450, 150 )
+lengthOfBaseATextField.id = "length textField"
+
+local lengthOfBaseBTextField = native.newTextField( display.contentCenterX - 400, display.contentCenterY - 100, 450, 150 )
+lengthOfBaseBTextField.id = "length textField"
+
+local lengthOfHeightTextField = native.newTextField( display.contentCenterX - 400, display.contentCenterY + 100, 450, 150 )
+lengthOfHeightTextField.id = "length textField"
+
+local areaOfTrapzoidTextField = display.newText( "Answer", display.contentCenterX, display.contentCenterY + 400, native.systemFont, 150 )
+areaOfTrapzoidTextField.id = "areaOfTrapzoidTextField"
+areaOfTrapzoidTextField:setFillColor( 1, 1, 1 )
+
+
+-- the click button
+local calculateButton = display.newImageRect( "./assets/sprites/enterButton.png", 406, 157 )
+calculateButton.x = display.contentCenterX +1000
+calculateButton.y = display.contentCenterY +800
+calculateButton.id = "calculate button"
+
+local function calculateButtonTouch( event )
+    -- this function calculates the area of a trapizoid given the length of one of its sides
+ 
+    local lengthOfBaseA
+	local lengthOfBaseB 
+	local lengthOfHeight
+	local areaOfTrapzoid
+ 
+    lengthOfBaseA = lengthOfBaseATextField.text
+    lengthOfBaseB = lengthOfBaseBTextField.text
+    lengthOfHeight = lengthOfHeightTextField.text
+    areaOfTrapzoid = (lengthOfBaseA + lengthOfBaseB) / 2 * lengthOfHeight
+    -- print( area of trapizoid )
+    areaOfTrapzoidTextField.text = "The area is " .. areaOfTrapzoid.." cm^2"
+
+    return true
+end
+
+calculateButton:addEventListener( "touch", calculateButtonTouch )
+
